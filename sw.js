18c719b6dafb1134e6390fea610db98674c7f09c
BrianACRM <brianbyrdac@gmail.com>
2026-02-15 09:30:09 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,15 @@
-const CACHE_NAME = 'ac-board-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './sw.js'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
+// Minimal service worker - no fetch interception (fixes iOS Safari FetchEvent TypeError bug)
+// iOS 16.4+ and 17+ have a known WebKit bug that causes "FetchEvent.respondWith TypeError: Internal error"
+// The safest fix for a self-contained single-file PWA is to skip fetch handling entirely.
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchRes => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, fetchRes.clone());
-          return fetchRes;
-        });
-      });
-    }).catch(() => caches.match('./index.html'))
+const CACHE_NAME = 'ac-board-v3';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['./index.html']);
+    }).catch(() => {})
   );
 });
 
@@ -29,7 +17,11 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
+
+// NO fetch handler - this is intentional.
+// Safari has a critical bug with FetchEvent.respondWith that causes PWAs to show
+// a blank screen or "Internal error" on launch from home screen.
+// Since this app is self-contained in index.html, it works perfectly without SW caching.
